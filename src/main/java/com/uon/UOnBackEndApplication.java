e6ce860b42e7bbd82a96f2f956f63b3f9a48bff1
SeongJeong <jsj046@naver.com>
2024-06-10 18:30:28 +0900

    [init]

---
+++ src/main/java/com/uon/UOnBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.uon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UOnBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UOnBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uon/UOnBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UOnBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
